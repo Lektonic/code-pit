35e081340cef59dc94a5274fd32f4355a475defe
Lektonic <lektonic@lektonic.com>
2024-07-15 10:01:45 -0500

    Added Init script for database
    Technically, moved the file and then embeded it into the program via
    @embedFile zig comptime.

--- src/main.zig
+++ src/main.zig
@@ -4,9 +4,23 @@ const c = @cImport({
 });
 const hash_digest = std.crypto.hash.sha2.Sha256;
 
+pub fn find_pit_file() []u8 {}
+
+pub fn open_code_db(path_to_db: [*c]const u8) ?*c.sqlite3 {
+    var c_db: ?*c.sqlite3 = undefined;
+    if (c.SQLITE_OK != c.sqlite3_open(path_to_db, &c_db)) {
+        std.debug.print("Unable to open database: {s}\n", .{c.sqlite3_errmsg(c_db)});
+        return c_db;
+    }
+    return c_db;
+}
+
+const code_pit_init_scrpt = @embedFile("code-pit.init.sql");
+
 pub fn main() !void {
     const version = c.sqlite3_libversion();
     std.debug.print("libsqlite3 version is {s}\n", .{version});
+    std.debug.print(code_pit_init_scrpt, .{});
 }
 
 pub fn hash_file(name: []u8) ![hash_digest.digest_length]u8 {
